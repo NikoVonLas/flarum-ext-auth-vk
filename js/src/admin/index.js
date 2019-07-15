69cd0b3b8106b85637b5e5f3e969e4ab1dd36fe2
Denis Nikolas <design.by.niko.las@gmail.com>
2019-07-15 23:49:18 +0500

    Bugfix for settings in admin dashboard

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,5 +1,5 @@
 import VkSettingsModal from './components/VkSettingsModal';
 
 app.initializers.add('flarum-auth-vk', () => {
-  app.extensionSettings['flarum-auth-vk'] = () => app.modal.show(new VkSettingsModal());
+  app.extensionSettings['nikovonlas-auth-vk'] = () => app.modal.show(new VkSettingsModal());
 });
