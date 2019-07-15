7d6b2dbf3d887784a911441f55f8e62cc7c44a3f
Denis Nikolas <design.by.niko.las@gmail.com>
2019-07-16 04:03:01 +0500

    Added transliteration on cyrillic username

--- src/VkAuthController.php
+++ src/VkAuthController.php
@@ -101,7 +101,7 @@ class VkAuthController implements RequestHandlerInterface
               'vk', $response['user_id'],
               function (Registration $registration) use ($user) {
                   $registration->provideTrustedEmail($user['email'])
-                      ->suggest('username', $user['username'])
+                      ->suggestUsername($this->translit($user['username']))
                       ->setPayload($user['payload']);
                   if (!empty($user['avatar'])) {
                     $registration->provideAvatar($user['avatar']);
@@ -109,5 +109,18 @@ class VkAuthController implements RequestHandlerInterface
               }
           );
         }
+    }
+
+    private function translit($s) {
+      $s = (string) $s; // преобразуем в строковое значение
+      $s = strip_tags($s); // убираем HTML-теги
+      $s = str_replace(array("\n", "\r"), ' ', $s);
+      $s = trim($s);
+      $s = function_exists('mb_strtolower') ? mb_strtolower($s) : strtolower($s);
+      $s = strtr($s, array('а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'j','з'=>'z','и'=>'i','й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n','о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'c','ч'=>'ch','ш'=>'sh','щ'=>'shch','ы'=>'y','э'=>'e','ю'=>'yu','я'=>'ya','ъ'=>'','ь'=>''));
+      $s = preg_replace('/[^0-9a-z-_ ]/i', '', $s);
+      $s = preg_replace('/\s+/', ' ', $s);
+      $s = str_replace(' ', '-', $s);
+      return $s;
     }
 }
